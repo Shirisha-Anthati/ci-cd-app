449a9a814f1d6e1e552f86a72f6200c85dbad692
Shirisha <anthatishirisha6185@gmail.com>
2023-02-02 21:09:35 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+    console.log(req.headers.host)
+    res.end('Hello world');
+}).listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});

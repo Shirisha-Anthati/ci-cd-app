fb36030368923097714a1afefc5af4cba9a89ead
Shirisha <anthatishirisha6185@gmail.com>
2023-02-14 17:31:13 +0530

    ccheds

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
     console.log(req.headers.host)
-    res.end(`Hello team, This is an example of deployment using ci-cd pipeline.`);
+    res.end(`Hello team, This is an example of deployment using ci-cd pipeline. welcome raja good to see you again`);
 }).listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
 });
